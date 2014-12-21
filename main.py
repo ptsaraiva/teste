c4e4b62503c7ccc7492ce61176bedf6ccbf7e6a2
ptsaraiva <ptsaraiva@outlook.com>
2014-12-21 00:00:10 +0000

    led blink

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 __author__ = 'Paulo'
 
-
 import RPi.GPIO as GPIO
+import time
+import json
+#import plotly.plotly as py
 
 print 'ola'
 
+pin = 19
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
 
-print 'ahh'
+GPIO.output(pin, True)
+time.sleep(5)
+GPIO.output(pin, False)
+time.sleep(5)
+
+print 'fim'
+
+GPIO.cleanup()
